7ba7969952d92a7658e7ba730c041462cf3fbcea
RahulSHinde <rvshinde12345@gmail.com>
2023-03-03 13:16:15 +0530

    [Harshada]: new code created

---
+++ src/small/New.java
@@ -0,0 +1,12 @@
+package small;
+
+public class New 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("New Code....");
+		System.out.println("This code use for push in git");
+		
+	}
+
+}
